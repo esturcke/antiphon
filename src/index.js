7931d472816351bcadb41f986f763259035643c2
Erik J. Sturcke <erik@sturcke.co>
2017-04-08 11:58:51 -0400

    Use minireset to reset CSS

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "minireset.css/minireset.css";
+import "./global.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
