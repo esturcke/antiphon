1d584acbcc4794447474f0685755282fcfe4b0ed
Erik J. Sturcke <erik@sturcke.co>
2017-04-08 12:07:56 -0400

    Center app contents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 // @flow
 import React from "react";
+import styled from "styled-components";
 
-const App = () => <div>Hello</div>;
+const Wrapper = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const App = () => <Wrapper>Hello</Wrapper>;
 
 export default App;
