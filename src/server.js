c9cdb2691292746b0ce8a45ce4bf09ed664de884
Erik J. Sturcke <erik@sturcke.co>
2017-04-10 08:18:02 -0400

    Remove express from flow

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const PORT = 3001;
+const SONGS = "./songs";
 
-app.get("/", function(req, res) {
+app.get("/songs", (req, res) => {
+  fs.readdir(SONGS, (err, files) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify(files));
+  });
+});
+
+app.get("/songs/:name", (req, res) => {
   res.send("Hello World!");
 });
 
