7cbb19aa0d87ad85871f507bdd9d8b831e00f1e6
Erik J. Sturcke <erik@sturcke.co>
2017-04-23 22:05:22 -0400

    Add hot module reloading

--- src/index.js
+++ src/index.js
@@ -5,4 +5,13 @@ import App from "./App"
 import "minireset.css/minireset.css"
 import "./global.css"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const root = document.getElementById("root")
+ReactDOM.render(<App />, root)
+
+if (module.hot) {
+  // $FlowFixMe: No webpack flow types
+  module.hot.accept("./App", () => {
+    const Next = require("./App").default
+    ReactDOM.render(<Next />, root)
+  })
+}
